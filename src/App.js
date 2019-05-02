6845954fb03d1fc01d66b538fe4b2aae3cfba3dc
Jorge Laj <jorge.laj@unitypromotores.com>
2019-05-02 15:03:22 -0600

    proyecto para buscar letras de canciones mediante una api con react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect,Fragment} from 'react';
+import Formulario from './componentes/Formulario';
+import Cancion from './componentes/Cancion';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+  //utilizar usestate con 3 states
+  const [artista, agregarArtista] = useState('');
+  const [letra,agregarLetra] = useState([]);
+  const [info, agregarInfo] = useState({});
+
+  //metodo para consultar la api de letras de canciones
+  const consultarApiLetra = async busqueda => {
+     const {artista,cancion} = busqueda;
+     const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+     //consultar la api
+     const resultado = await axios(url);
+     //almacenar el artista que se busco
+     agregarArtista(artista)
+     //almacenar la letra en el state
+     agregarLetra(resultado.data.lyrics);
+  }
+
+  //metodo para consultar la informacion del artista
+  const consultarApiInfo = async (artista) =>{
+    const url = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+    const consulta = await axios(url);
+    agregarInfo(consulta.data.artists);
+    console.log(info);
+    console.log(consulta.data);
+  }
+
+  useEffect( () =>{
+     consultarApiInfo();
+  },[artista])
+
+  return(
+    <Fragment>
+      <Formulario consultarApiLetra ={consultarApiLetra}/>
+      <div className="container mt5-5">
+        <div className="row">
+          <div className="col-md-6">
+          </div>
+          <div className="col-md-6">
+            <Cancion 
+              letra={letra}
+            />
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  )
 }
 
 export default App;
---
+++ src/componentes/Cancion.js
@@ -0,0 +1,14 @@
+import React,{Fragment} from 'react'
+
+
+function Cancion(props){
+    if(props.letra.length ===0) return null;
+    return(
+        <Fragment>
+           <h2>Letra Cancion</h2>
+           <p className="letra">{props.letra}</p>
+        </Fragment>
+    )
+}
+
+export default Cancion;
